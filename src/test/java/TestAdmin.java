ec84abcf52f8ebbbae7ce36c5ecd2e5763deb3c5
Selena Tuyen <sltuyen@ucdavis.edu>
2017-03-04 18:19:00 -0800

    Add changecapcity and createclass tests

--- src/test/java/TestAdmin.java
+++ src/test/java/TestAdmin.java
@@ -3,6 +3,9 @@ import api.core.impl.Admin;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by seest on 3/2/2017.
  */
@@ -14,16 +17,27 @@ public class TestAdmin {
         this.admin = new Admin();
     }
 
-    /**
-     * Adjust the capacity (maximum number of students) of class {@code className} to new capacity {@code capacity}
-     *
-     * @param className Name of the class for which capacity should be changed
-     * @param year Year in which this class is taught
-     * @param capacity New capacity of this class, must be at least equal to the number of students enrolled
-     */
     @Test
     public void testCreateClass(){
-        this.admin.createClass();
-        assertTrue
+        this.admin.createClass("Test", 2017, "Instructor", 15);
+        assertTrue(this.admin.classExists("Test", 2017));
+    }
+
+    @Test
+    public void testCreateClass2(){
+        this.admin.createClass("Test", 2016, "Instructor", 15);
+        assertFalse(this.admin.classExists("Test", 2016));
+    }
+
+    @Test
+    public void testChangeCapacity(){
+        this.admin.changeCapacity("Test", 2017, 53);
+        assertTrue(53 >= this.admin.getClassCapacity("Test", 2017));
+    }
+
+    @Test
+    public void testChangeCapacity2(){
+        this.admin.changeCapacity("Test", 2017, 4);
+        assertFalse(4 < this.admin.getClassCapacity("Test", 2017));
     }
 }
